9e53171c0aebad681c2668f43ae7a5107f763953
eande-jasonc1025 <67285598+eande-jasonc1025@users.noreply.github.com>
2022-07-26 22:42:37 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+// * Keep in mind that the micro:bit is 'updside-down' when using the LED 5x5 Matrix.
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        roboQuest.powerMotorsViaBlueRedBlackPins(PortGroup_BlueRedBlack__PortIds__Enum.S1_MotorLeft__S0_MotorRight, 100, 100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+        roboQuest.continueCurrentState_CountdownTimer_Set(3, Time_Units_Enum.Seconds)
+        basic.pause(3000)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+        roboQuest.powerMotorsViaBlueRedBlackPins(PortGroup_BlueRedBlack__PortIds__Enum.S1_MotorLeft__S0_MotorRight, 100, 100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+        roboQuest.continueCurrentState_CountdownTimer_Set(3, Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        roboQuest.powerMotorsViaBlueRedBlackPins(PortGroup_BlueRedBlack__PortIds__Enum.S1_MotorLeft__S0_MotorRight, -100, -100)
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        roboQuest.continueCurrentState_CountdownTimer_Set(3000, Time_Units_Enum.Milliseconds)
+    }
+    if (true) {
+        roboQuest.powerMotorsViaBlueRedBlackPins(PortGroup_BlueRedBlack__PortIds__Enum.S1_MotorLeft__S0_MotorRight, 100, -100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        roboQuest.continueCurrentState_CountdownTimer_Set(3, Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        roboQuest.powerMotorsViaBlueRedBlackPins(PortGroup_BlueRedBlack__PortIds__Enum.S1_MotorLeft__S0_MotorRight, -100, 100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+        roboQuest.continueCurrentState_CountdownTimer_Set(3000, Time_Units_Enum.Milliseconds)
+    }
+    if (true) {
+        roboQuest.powerMotorsViaBlueRedBlackPins(PortGroup_BlueRedBlack__PortIds__Enum.S1_MotorLeft__S0_MotorRight, 0, 0)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        roboQuest.continueCurrentState_CountdownTimer_Set(3, Time_Units_Enum.Seconds)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        roboQuest.powerMotorsViaBlueRedBlackPins(PortGroup_BlueRedBlack__PortIds__Enum.S1_MotorLeft__S0_MotorRight, 0, 0)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        roboQuest.continueCurrentState_CountdownTimer_Set(3, Time_Units_Enum.Seconds)
+    }
+})
+basic.showIcon(IconNames.SmallHeart)
+roboQuest.continueCurrentState_CountdownTimer_Set(3, Time_Units_Enum.Seconds)
+basic.showIcon(IconNames.Heart)
+roboQuest.continueCurrentState_CountdownTimer_Set(3000, Time_Units_Enum.Milliseconds)
+basic.showIcon(IconNames.Happy)
